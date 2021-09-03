cf60bae7120dd99a868ef411452be4e830516153
Tom Conder <tom@itstom.co>
2021-09-03 16:10:22 -0500

    set cameraFront on initialization

--- src/mountains.cpp
+++ src/mountains.cpp
@@ -85,13 +85,8 @@ bool Mountains::onUserUpdate(Uint32 elapsedTime) {
 
     sprite->render("coffee", glm::vec2(10.f, 10.f), glm::vec2(64.f, 64.f));
 
-    glm::vec3 pos = camera->getPosition();
-    std::stringstream tuple;
-    tuple.precision(2);
-    tuple <<" [" << std::fixed << pos.x << ", " << pos.y << ", " << pos.z << "]";
-
     OpenGLResourceManager::getFont("gothic")->
-        renderText(appName + tuple.str(), 25.0, 25.0, glm::vec3(0.5, 0.9f, 1.0f));
+        renderText(appName, 25.0, 25.0, glm::vec3(0.5, 0.9f, 1.0f));
 
     return true;
 }
